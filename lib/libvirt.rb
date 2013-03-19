eb872e57b9cb0e9569361bed29d73302a04b3d46
dima <pronix.service@gmail.com>
2013-03-20 00:25:45 +0100

    state to string

--- lib/libvirt.rb
+++ lib/libvirt.rb
@@ -37,4 +37,33 @@ module Libvirt
             "#{major}.#{minor}.#{release}"
         end
     end
+
+    class Domain
+      def state_s
+        s=self.state
+        s[0] # state
+        s[1] # reason
+        res=[]
+        res[0] = case s[0]
+        when 0
+          'no state'
+        when 1
+          'the domain is running'
+        when 2
+          'the domain is blocked on resource'
+        when 3
+          'the domain is paused by user'
+        when 4
+          'the domain is being shut down'
+        when 5
+          'the domain is shut off'
+        when 6
+          'the domain is crashed'
+        when 7
+          'the domain is suspended by guest power management'
+        when 8
+          'NB: this enum value will increase over time as new events are added to the libvirt API. It reflects the last state supported by this version of the libvirt API.'
+        end
+      end
+    end
 end
